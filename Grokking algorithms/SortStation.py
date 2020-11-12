f8751ec29e2ca7377c9c071e06994801d193e7a6
sharkeeyy <sfmei2003@yandex.ru>
2020-11-12 13:04:36 +0300

    ready to roll

--- Grokking algorithms/SortStation.py
+++ Grokking algorithms/SortStation.py
@@ -7,10 +7,22 @@ def is_digit(string):
     return False
 
 
+def is_sign(string):
+    if string in ['(', ')', '+', '-', '/', '*']:
+        return True
+    return False
+
+
+def is_equals(string):
+    if string in ['=']:
+        return True
+    return False
+
+
 def get_tokens(string):
     tokens = []
     i = 0
-    while i < len(string) - 1:
+    while i <= len(string) - 1:
         if not is_digit(string[i]):
             tokens.append(string[i])
             i += 1
@@ -19,7 +31,7 @@ def get_tokens(string):
             while is_digit(string[i]):
                 digit += string[i]
                 i += 1
-            tokens.append(digit)
+            tokens.append(int(digit))
 
     return tokens
 
@@ -28,4 +40,30 @@ if __name__ == "__main__":
     str = "5+6*200-(11+60/30)*3="
     print(str)
 
-    print(get_tokens(str))
+    tokens = get_tokens(str)
+    print(tokens)
+
+    digits = []
+    signs = []
+    result = 0
+
+    for token in tokens:
+        if not is_sign(token):
+            print(token + 'is sign')
+        elif token == '+':
+            pass
+        elif token == '-':
+            pass
+        elif token == '*':
+            pass
+        elif token == '/':
+            pass
+        elif token == '(':
+            pass
+        elif token == ')':
+            pass
+        elif token == '=':
+            pass
+
+
+
