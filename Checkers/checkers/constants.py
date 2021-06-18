dd1d8a440c39c175e077bd135f9278a901b25f52
sharkeeyy <sfmei2003@yandex.ru>
2021-06-18 12:20:12 +0300

    a board

--- Checkers/checkers/constants.py
+++ Checkers/checkers/constants.py
@@ -1,6 +1,6 @@
 import pygame
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 400, 400
 ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH // ROWS
 
@@ -9,3 +9,4 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+GREEN = (47, 145, 47)
