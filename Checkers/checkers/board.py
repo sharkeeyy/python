c558b482f419c1a8fb54785370326b6c4c16e434
sharkeeyy <sfmei2003@yandex.ru>
2021-06-23 15:15:53 +0300

    and we are moving

--- Checkers/checkers/board.py
+++ Checkers/checkers/board.py
@@ -19,6 +19,20 @@ class Board:
             for col in range(row % 2, COLS, 2):
                 pygame.draw.rect(win, GREEN, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    def move(self, piece, row, col):
+        self.board[row][col], self.board[piece.row][piece.col] = self.board[piece.row][piece.col], self.board[row][col]
+        piece.move(row, col)
+
+        if row == ROWS or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1
+
+    def get_piece(self, row, col):
+        return self.board[row][col]
+
     def create_board(self):
         for row in range(ROWS):
             self.board.append([])
--- Checkers/main.py
+++ Checkers/main.py
@@ -1,11 +1,18 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
 from checkers.board import Board
 
 FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
+
+
+def get_coordinates_from_mouse_click(position):
+    x, y = position
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 
 def main():
@@ -21,7 +28,10 @@ def main():
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                position = pygame.mouse.get_pos()
+                row, col = get_coordinates_from_mouse_click(position)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
 
         board.draw(WIN)
         pygame.display.update()
